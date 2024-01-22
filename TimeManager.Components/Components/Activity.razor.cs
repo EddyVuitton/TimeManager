59dba0a31ab5b0d0870c319c89e0c03fb808849f
Edward Gadawski <edwardgadawski@gmail.com>
2024-01-22 10:58:36 +0100

    Dodanie aktywności na dzień

--- TimeManager.Components/Components/Activity.razor.cs
+++ TimeManager.Components/Components/Activity.razor.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using TimeManager.Components.Dialogs;
 using TimeManager.Data.DTOs;
 
 namespace TimeManager.Components.Components;
 
 public partial class Activity
 {
+    [Inject] public IDialogService? DialogService { get; set; }
+
     [Parameter] public List<ActivityDto>? ActivitiesDto { get; set; }
 
     protected override void OnInitialized()
     {
+    }
+
+    private void OpenDialog()
+    {
+        //To do...
     }
 }
