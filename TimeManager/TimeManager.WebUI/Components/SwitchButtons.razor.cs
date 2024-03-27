a2f60a29f49d0c1f7b648e7ac441dc3ab5d8a832
Edward Gadawski <edwardgadawski@gmail.com>
2024-03-27 16:31:00 +0100

    Dalszy rozwój strony z zadaniami

---
+++ TimeManager/TimeManager.WebUI/Components/SwitchButtons.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace TimeManager.WebUI.Components;
+
+public partial class SwitchButtons
+{
+    [Inject] public NavigationManager NavigationManager { get; set; } = null!;
+
+    private Variant _calendarVariant = Variant.Filled;
+    private Variant _listVariant = Variant.Outlined;
+
+    private void SwitchToCalendar()
+    {
+        _calendarVariant = Variant.Filled;
+        _listVariant = Variant.Outlined;
+        NavigationManager.NavigateTo("/");
+    }
+
+    private void SwitchToTasks()
+    {
+        _calendarVariant = Variant.Outlined;
+        _listVariant = Variant.Filled;
+        NavigationManager.NavigateTo("/tasks");
+    }
+}
---
+++ TimeManager/TimeManager.WebUI/Pages/Tasks.razor.cs
@@ -0,0 +1,34 @@
+﻿namespace TimeManager.WebUI.Pages;
+
+public partial class Tasks
+{
+    private readonly List<CustomList> lists = [];
+
+    protected override void OnInitialized()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            lists.Add(new CustomList() { ID = i, IsChecked = false });
+        }
+    }
+
+    private void AddList()
+    {
+        var newId = lists.Max(x => x.ID) + 1;
+
+        lists.Add(new CustomList() { ID = newId, IsChecked = false });
+    }
+
+    private void AddTask(int id)
+    {
+        lists.First(x => x.ID == id).Tasks.Add("Zadanie");
+    }
+
+    private class CustomList
+    {
+        public int ID { get; init; }
+        public bool IsChecked { get; set; }
+
+        public List<string> Tasks { get; set; } = [];
+    }
+}
